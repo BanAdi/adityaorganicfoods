3f5a68dfbd06f356d345fc27e2f2e3257ba5e410
Aditya <ge64nej@mytum.de>
2026-01-19 20:21:20 +0100

    initial website

---
+++ Assets/js/ main.js
@@ -0,0 +1,5 @@
+const els = document.querySelectorAll(".reveal");
+const io = new IntersectionObserver((entries)=>{
+  entries.forEach(e=>{ if(e.isIntersecting) e.target.classList.add("show"); });
+},{threshold:0.12});
+els.forEach(el=>io.observe(el));
